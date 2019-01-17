12a24c8a0c4afa24c28c65088d2c931b2a5b7879
georgeyang <georgeyang@chinasofti.com>
2019-01-17 15:54:20 +0800

    remove id set

--- src/main/java/com/csi/sbs/sysadmin/business/controller/SysTransactionLogController.java
+++ src/main/java/com/csi/sbs/sysadmin/business/controller/SysTransactionLogController.java
@@ -9,16 +9,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.csi.sbs.common.business.util.UUIDUtil;
 import com.csi.sbs.sysadmin.business.entity.SysTransactionLogEntity;
 import com.csi.sbs.sysadmin.business.service.SysTransactionLogService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.swagger.annotations.Api;
+
 @CrossOrigin // 解决跨域请求
 @Controller
 @RequestMapping("/sysadmin/log")
+@Api(value="The controller is log create")
 public class SysTransactionLogController {
 
 	@Resource
@@ -30,10 +31,8 @@ public class SysTransactionLogController {
 	@ResponseBody
 	public String createTransactionLog(@RequestBody SysTransactionLogEntity stl) throws JsonProcessingException {
 		Map<String, Object> map = new HashMap<String, Object>();
-		try {
-			stl.setId(UUIDUtil.generateUUID());
-			stlservice.writeTransactionLog(stl);
-			
+		try {			
+			stlservice.writeTransactionLog(stl);			
 			map.put("msg", "日志插入成功");
 			map.put("code", "1");
 		} catch (Exception e) {
