475e17394c46f38b4408d51b9f370b25f096101f
dapengchina <aoqishequ@163.com>
2019-02-20 16:23:28 +0800

    Change-Id: I2dbbf9c3cb6e3926df18dc517877bbebcd8feb0f

---
+++ src/main/java/com/csi/sbs/sysadmin/business/clientmodel/ReUserModel.java
@@ -0,0 +1,35 @@
+package com.csi.sbs.sysadmin.business.clientmodel;
+
+public class ReUserModel {
+
+	private String userid;
+
+	private String username;
+
+	private String email;
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
