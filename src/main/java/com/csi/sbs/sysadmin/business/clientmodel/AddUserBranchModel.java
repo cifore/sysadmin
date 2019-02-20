fe13a7a39bae0067145e4611a1b192cd3ec8f8cb
dapengchina <aoqishequ@163.com>
2019-02-20 15:44:59 +0800

    Change-Id: I100af9c27437480a5c1dd54ff47796610fef1675

---
+++ src/main/java/com/csi/sbs/sysadmin/business/clientmodel/AddUserBranchModel.java
@@ -0,0 +1,25 @@
+package com.csi.sbs.sysadmin.business.clientmodel;
+
+public class AddUserBranchModel {
+
+	private String userid;
+
+	private String bankid;
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getBankid() {
+		return bankid;
+	}
+
+	public void setBankid(String bankid) {
+		this.bankid = bankid;
+	}
+
+}
---
+++ src/main/java/com/csi/sbs/sysadmin/business/controller/UserBranchController.java
@@ -0,0 +1,55 @@
+package com.csi.sbs.sysadmin.business.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import com.csi.sbs.sysadmin.business.clientmodel.AddUserBranchModel;
+import com.csi.sbs.sysadmin.business.service.UserBranchService;
+import com.csi.sbs.sysadmin.business.util.ResultUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.annotations.ApiOperation;
+
+
+@CrossOrigin // 解决跨域请求
+@Controller
+@RequestMapping("/sysadmin/userbranch")
+public class UserBranchController {
+	
+	@Resource
+	private UserBranchService userBranchService;
+	
+	@Resource
+	private RestTemplate restTemplate;
+
+	ObjectMapper objectMapper = new ObjectMapper();
+	
+	
+	/**
+	 * 增加userbranch
+	 * 
+	 * @throws Exception
+	 */
+	@SuppressWarnings("rawtypes")
+	@RequestMapping(value = "/addUserBranch", method = RequestMethod.POST)
+	@ResponseBody
+	@ApiOperation(value = "This API is designed to add a userbranch.", notes = "version 0.0.1")
+	public ResultUtil addUserBranch(@RequestBody AddUserBranchModel addUserBranchModel) throws Exception {
+		try {
+			return userBranchService.addUserBranch(addUserBranchModel, restTemplate);
+		} catch (Exception e) {
+			ResultUtil result = new ResultUtil();
+			result.setCode("0");
+			result.setMsg("fail");
+			throw new RuntimeException(objectMapper.writeValueAsString(result));
+		}
+	}
+
+}
