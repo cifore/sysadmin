b3405eab936ee79b8d6e6ba5dc0f772ddb0180d5
dapengchina <aoqishequ@163.com>
2019-05-30 11:59:42 +0800

    Change-Id: I5748eed5d7e384ae5a756eeae98d78611fa006b4

--- src/main/java/com/csi/sbs/sysadmin/business/constant/PathConstant.java
+++ src/main/java/com/csi/sbs/sysadmin/business/constant/PathConstant.java
@@ -7,7 +7,7 @@ public class PathConstant {
 	//获取内部服务调用地址
 	public static final String SERVICE_INTERNAL_URL = "http://" + CommonConstant.getSYSADMIN() + SysConstant.SERVICE_INTERNAL_URL;
 	//网关地址(服务器)
-    public static final String GATEWAY_SERVICE = "117.78.38.89:8086/sysadmin";
+    public static final String GATEWAY_SERVICE = "47.112.146.96:8086/sysadmin";
     //网关地址(本地)
     public static final String GATEWAY_LOCALHOST = "localhost:8086/sysadmin";
     //获取customer服务地址
@@ -15,7 +15,7 @@ public class PathConstant {
     //创建customer 服务地址
     public static final String CREATE_CUSTOMER_URL = "http://DEPOSIT/deposit/account/customerCreation";
     //t_customer_master模板地址
-    public static final String CUSTOMER_TEMPLATE = "D://t_customer_master.xls";
+    public static final String CUSTOMER_TEMPLATE = "C://t_customer_master.xls";
     //创建Account 服务地址
     public static final String CREATE_ACCOUNT_URL = "http://DEPOSIT/deposit/account/accountCreation";
     //获取下一个可用的递增序列号服务地址
